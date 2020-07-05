ea42cc97d0e35e944e64024b0a3462e486bd4b66
Hariharan Ramanan <harryharanr@gmail.com>
2020-07-05 22:44:37 +0530

    components added inside equipment modules

---
+++ src/app/main/equipment/components/add-equipment/equipment-information/equipment-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-information',
+  templateUrl: './equipment-information.component.html',
+  styleUrls: ['./equipment-information.component.scss']
+})
+export class EquipmentInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/main/equipment/components/add-equipment/spare-information/spare-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spare-information',
+  templateUrl: './spare-information.component.html',
+  styleUrls: ['./spare-information.component.scss']
+})
+export class SpareInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
