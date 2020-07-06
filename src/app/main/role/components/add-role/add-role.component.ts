afa7c32e8dfc79f38a66a2047c6908522712f476
Hariharan Ramanan <harryharanr@gmail.com>
2020-07-06 16:23:19 +0530

    components added in role modules

--- src/app/main/role/components/add-role/add-role.component.ts
+++ src/app/main/role/components/add-role/add-role.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { MatBottomSheetRef } from "@angular/material/bottom-sheet";
 
 @Component({
-  selector: 'app-add-role',
-  templateUrl: './add-role.component.html',
-  styleUrls: ['./add-role.component.scss']
+    selector: "app-add-role",
+    templateUrl: "./add-role.component.html",
+    styleUrls: ["./add-role.component.scss"],
 })
 export class AddRoleComponent implements OnInit {
+    addRoleForm: FormGroup;
+    @ViewChild("form") form;
 
-  constructor() { }
+    constructor(
+        private _bottomSheetRef: MatBottomSheetRef<AddRoleComponent>,
+        private formBuilder: FormBuilder
+    ) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit() {
+        this.createForm();
+    }
 
+    permissions: any[] = [
+        { value: "Shopfloor Lead" },
+        { value: "Plant Lead" },
+        { value: "Storekeeper" },
+    ];
+
+    createForm() {
+        this.addRoleForm = this.formBuilder.group({
+            roleName: ["", Validators.required],
+            permissions: ["", Validators.required],
+        });
+    }
+
+    cancel(): void {
+        this.addRoleForm.reset();
+        this._bottomSheetRef.dismiss();
+    }
+
+    addRole(): void {
+        this.form.resetForm();
+        this._bottomSheetRef.dismiss({ test: "wow" });
+    }
 }
